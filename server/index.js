9775ad2fb1af28f895d5572c9e99632ca2cd842c
nasimulhaquepathan <nasimul194@gmail.com>
2023-05-21 02:56:21 +0600

    first commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const register = require("./routes/register");
 const login = require("./routes/login");
+
 // const orders = require("./routes/orders");
 // const stripe = require("./routes/stripe");
 // const productsRoute = require("./routes/products");
@@ -10,7 +11,6 @@ const login = require("./routes/login");
 const products = require("./products");
 
 const app = express();
-
 require("dotenv").config();
 
 app.use(express.json());
@@ -18,6 +18,7 @@ app.use(cors());
 
 app.use("/api/register", register);
 app.use("/api/login", login);
+
 // app.use("/api/orders", orders);
 // app.use("/api/stripe", stripe);
 // app.use("/api/products", productsRoute);
