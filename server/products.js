1e1d28fd05989edc7f953cb9f9897a880b95327b
nasimulhaquepathan <nasimul194@gmail.com>
2023-05-20 16:19:28 +0600

    first commit

---
+++ server/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+      id: 1,
+      name: "iPhone 12 Pro",
+      brand: "Apple",
+      desc: "6.1-inch display",
+      price: 999,
+      image:
+      "https://res.cloudinary.com/dgq3uqxy5/image/upload/v1684267921/online-folder/galaxyS_u9r5hm.png",
+    },
+    {
+      id: 2,
+      name: "iPhone 12",
+      brand: "Apple",
+      desc: "5.4-inch mini display",
+      price: 699,
+      image:
+        "https://res.cloudinary.com/dgq3uqxy5/image/upload/v1684267908/online-folder/iphone12_qjn5ob.jpg",
+    },
+    {
+      id: 3,
+      name: "Galaxy S",
+      brand: "Samsung",
+      desc: "6.5-inch display",
+      price: 399,
+      image:
+        "https://res.cloudinary.com/dgq3uqxy5/image/upload/v1684267894/online-folder/iphone12pro_dzmvpi.jpg",
+    },
+  ];
+  
+  module.exports = products;
